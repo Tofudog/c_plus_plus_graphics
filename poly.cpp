7511b995581ed5ac0ff3a796565d133922481697
leode <leodefarias25@gmail.com>
2022-05-28 12:01:27 -0500

    hopefully updated poly.cpp now

---
+++ poly.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Graphics {
+    public:
+        string library;
+        string shapes;
+};
+
+int main() {
+    
+    cout << "Hello World";
+    return 0;
+}
